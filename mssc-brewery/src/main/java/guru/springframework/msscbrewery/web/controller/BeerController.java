dca8ab630a28771bdace661c1d9b72e4cebd8f67
SwAppCoder <iizdepskyy112@gmail.com>
2021-05-07 10:30:46 +0300

    34_HTTP_DELETE_with_Spring_MVC

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -26,7 +26,7 @@ public class BeerController {
     }
 
     @PostMapping  // POST - create new beer
-    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+    public ResponseEntity handlePost(BeerDto beerDto) {
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -39,11 +39,16 @@ public class BeerController {
     }
 
     @PutMapping ({"/{beerId}"})
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 
+    @DeleteMapping ({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 }
