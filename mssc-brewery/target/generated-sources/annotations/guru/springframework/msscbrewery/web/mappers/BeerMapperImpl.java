7821d256974bfb1bb82451c0a7bde81ed7e2a531
SwAppCoder <iizdepskyy112@gmail.com>
2021-05-08 22:01:15 +0300

    82_LC_Implementing_MapStruct

--- mssc-brewery/target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
+++ mssc-brewery/target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-08T21:34:46+0300",
+    date = "2021-05-08T22:00:23+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
