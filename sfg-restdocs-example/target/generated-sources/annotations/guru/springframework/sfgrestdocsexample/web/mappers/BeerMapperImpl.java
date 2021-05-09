e75f0dda575bd49bc307a58f68db818e1c0be77d
SwAppCoder <iizdepskyy112@gmail.com>
2021-05-09 13:58:13 +0300

    96_URI_Customization

--- sfg-restdocs-example/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ sfg-restdocs-example/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-09T11:43:55+0300",
+    date = "2021-05-09T13:55:46+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
