719f4e8aa440b049f20e7555231e246883f003bc
SwAppCoder <iizdepskyy112@gmail.com>
2021-05-11 22:35:27 +0300

    147_beer_service_parent_POM_configuration

--- mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-10T23:29:25+0300",
+    date = "2021-05-11T22:30:43+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
